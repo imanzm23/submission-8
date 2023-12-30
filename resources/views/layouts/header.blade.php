286ddd0409bb09dff4ebcd2e51652f2ec64116e2
imanzm23 <mantox66@gmail.com>
2023-12-30 14:56:08 +0700

    submission_8

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="Saquib" content="Blade">
+<title>Perpustakaan Digital</title>
+<!-- load bootstrap from a cdn -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
